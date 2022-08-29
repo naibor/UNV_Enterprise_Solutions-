a82d7750edb4c4e84bb21996e96cd3e23dacac6d
naibor <liznaibor.en@gmail.com>
2022-08-30 01:07:46 +0300

    create views for api

--- EnterpriseSolution/app/views.py
+++ EnterpriseSolution/app/views.py
@@ -1,8 +1,17 @@
+# django imports
+from audioop import reverse
 from django.http import HttpResponse
-# from django.template import loader
 from django.shortcuts import render, get_object_or_404
+from django.views.generic.edit import UpdateView, DeleteView
+from django.views.generic.detail import DetailView
 
-from .models import Project
+# rest framework
+from rest_framework.generics import ListAPIView
+
+# local imports
+from .serializer import ProjectImplementationSerializer, ProjectSerializer
+from .models import Project,ProjectImplementation
+
 #  index page view all projects(get)
 def index(request):
     if request.method == "POST":
@@ -15,20 +24,59 @@ def index(request):
         return render(request, 'app/index.html', context)
 
 # view one project
-def read(request, project_id):
-    project = get_object_or_404(Project, pk=project_id)
-    return render(request, 'app/project_detail.html', {'project': project})
+# def read(request, project_id):
+#     project = get_object_or_404(Project, pk=project_id)
+#     context = {'project': project}
+#     return render(request, 'app/project_detail.html', context)
+
+class ProjectDetailView(DetailView):
+    model = Project
+
+# # update a project
+class ProjectUpdateView(UpdateView):
+    model = Project
+    fields = '__all__'
+    
+    # def get_success_url(self) -> str:
+    #     return reverse('app:')
+    # success_url: "/"
+
+# # delete a project
+
+class ProjectDeleteView(DeleteView):
+    model=Project
+
+    def get_success_url(self) -> str:
+        return reverse('app:index')
+
+
+# API 
+# get all projects
+class ProjectViewAll(ListAPIView):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+    lookup_field = "pk"
+
+# get projects based on a country
+class CountryProjectViewAll(ListAPIView):
+    queryset = ProjectImplementation.objects.all()
+    serializer_class = ProjectImplementationSerializer
+
+    def get_queryset(self):
+        country = self.kwargs.get('country',None)
+        if country:
+            return ProjectImplementation.objects.filter(country_id__country=country).all()
+        return ProjectImplementation.objects.all()
+
     
 
-# create a project (post)
-# def create(request):
-#     return HttpResponse("CREATE A PROJECTS GO HERE!!!")
-#     pass
-# # update a project
-# def update(request, project_id):
-#     return HttpResponse(" UPDATE A PROJECTS GO HERE!!!")
-#     pass
-# # delete a project
-# def delete(request, project_id):
-#     return HttpResponse("DELETE A PROJECTS GO HERE!!!")
-#     pass
+# get projects based on the status
+class ProjectStatusViewAll(ListAPIView):
+    quesyset = Project.objects.all()
+    serializer_class = ProjectSerializer
+    
+
+    def get_queryset(self):
+        status = self.kwargs.get('status','')
+        return Project.objects.filter(status=status).all()
+    
