76ef1a618e9f113f909fb759e721e65966f9f719
naibor <liznaibor.en@gmail.com>
2022-08-29 20:51:02 +0300

    update views to include get all project and get single project

--- EnterpriseSolution/app/views.py
+++ EnterpriseSolution/app/views.py
@@ -1,20 +1,18 @@
 from django.http import HttpResponse
 # from django.template import loader
 from django.shortcuts import render, get_object_or_404
-from django.http import Http404
 
 from .models import Project
 #  index page view all projects(get)
 def index(request):
     project_list = Project.objects.all()
-    # template = loader.get_template('app/index.html')
     context = {'project_list': project_list}
     return render(request, 'app/index.html', context)
 
 # view one project
 def read(request, project_id):
     project = get_object_or_404(Project, pk=project_id)
-    return render(request, 'app/detail.html', {'project': project})
+    return render(request, 'app/project_detail.html', {'project': project})
     
 
 # create a project (post)
