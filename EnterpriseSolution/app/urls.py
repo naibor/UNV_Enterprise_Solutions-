94b0824c7cf5c927b5882cf64f7d186225f7ddeb
naibor <liznaibor.en@gmail.com>
2022-08-30 01:08:50 +0300

    create url for api

--- EnterpriseSolution/app/urls.py
+++ EnterpriseSolution/app/urls.py
@@ -1,14 +1,21 @@
 from django.urls import path
-from . import views
+from .views import ProjectDetailView, ProjectUpdateView, ProjectDeleteView,index, ProjectViewAll, CountryProjectViewAll, ProjectStatusViewAll
+app_name ="app"
 
 urlpatterns = [
-    # view all projects 
-    path('', views.index, name='index'),
-    # view one project
-    path('<int:project_id>/', views.read, name='read'),
     # create a project
+    # view all projects 
+    path('', index, name='index'),
+    # view one project
+    path('<pk>/', ProjectDetailView.as_view(), name='read'),
     # delete a project
-    # path('<int:project_id>/', views.delete, name='delete'),
+    path('<pk>/delete', ProjectDeleteView.as_view(), name='delete'),
     # update a project
-    # path('<int:project_id>/', views.update, name='update')
+    path('<pk>/edit', ProjectUpdateView.as_view(), name='update'),
+
+    # api urls
+    path('api/projects/all', ProjectViewAll.as_view(), name='api-projects-all' ),
+    path('api/projects/country/<str:country>', CountryProjectViewAll.as_view(), name='api-projects-country-all' ),
+    path('api/projects/status/<str:status> ', ProjectStatusViewAll.as_view(), name='api-projects-status-all' )
+ 
 ]
